ef1995420e9180824bf9707263dfe9fcf9e49d83
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-03-19 16:10:21 +0700

    add date pipe

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -11,7 +11,7 @@ export class ReservationService {
     constructor(private _http: HttpClient) { }
 
     getReservation () {      
-        let token = 'EkEj4WRR4F0uvWfdYDSOkBwFG79G1JRc1SwqPR3vsq2B_Rm8S268SMvEEcrH9DKE1KKDeQNcDtNDthLHGWyRJk9rU5QrpzjiPpDwWMlHKQKssXXqUsamL6DQbIqvQVk2BT84h8MFQnS0tX3A1HQqKyjSHJz4RNtNfB0e2DQHSjxj-aOai2EHHmU9rnHiADDjAStgo4mkf-lzUKmZ9_SdHKQtMokir1FoRT4y9bmDSJt_i3kiTR8dfdLac0QwFwElvF7EFAnZb_c_O1Rak8Ai5i00I2IxN3FUpi0nKBvaPo3mvsBNaBQbUnRpIySJFhegKAOkvYNyAKMcr9EyzQEwX-IjEwttfAY9tOmnyVuXVawnONmuT3rx_vdIcqlmKjILYONGnl-Wrjrpgv0Ijol5nw0flw3GLE-HJpaVJAMws5Z2Pyu9HKW4mq0_TgDlbzPzc2yxNZPo_upCPjUI2akUb5n69-osltysrlFPhek02cqq1TDqD5cr3qEmNUNgMWt8';
+        let token = 'gZArob3TGziwXTlnMCEteDYiRbYrP2L6meGpWFExM2rE3w9dl2jhcnJaHAIOdxApaynuYMuQIT206-qBAdhKbTAhFrwEizp5xaU4wVWqGWiQpZZEe_CkHd1DE27m-4FWf5eVEFOZbxfkAvlSjpZYxrrTj1mY0BKWgdsFl1JbTLaIE9knj2ShW9FoJ_1xHTzGMfE4ZFX23QDfVCEq8fNxtfulKZUHvTpnfb9BgIuNb0hmpgbRiesBRo_YePav6OfeXgH_VL1SV6gP77O28EG260PO35mj1RpfnDDG7Rctqs_N1Rx_irBfK7BwUoD8at2n01uRO2W9f-cAPq37MoIFMvcHnYt-Ze0RKR3hAXNZUEW2OYaXt3JlwJWG23nK59ZnlyHgAoQAX7lx3L7mNltm1RJEVxaY1YGpDxfumRMggMrQM1Hcq39hlOnJnUj2ph8WwfbcmtxqeTgqDktnyuc6hff2twBvd-QOhsVipM9RwlmujSmEYfgarYKN5jbqaGjn';
         return this._http
             .get(this._url, {
                 params: new HttpParams().set('query', ' '),
